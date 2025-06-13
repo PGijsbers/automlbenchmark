fac0d3ff8a527f158637391266d586c320ef646f
Pieter Gijsbers <p.gijsbers@tue.nl>
2025-06-13 10:35:38 +0200

    mimic exact input

--- ag_mwe.py
+++ ag_mwe.py
@@ -1,5 +1,7 @@
 from autogluon.tabular import TabularDataset, TabularPredictor
 import sys
+import os
+import tempfile
 
 _, time, train, test = sys.argv
 
@@ -7,12 +9,10 @@ train_data = TabularDataset(train)
 test_data = TabularDataset(test)
 
 print("Training AutoGluon")
-predictor = TabularPredictor(
-    label='Class', verbosity=2, eval_metric='log_loss',
-).fit(
-    train_data=train_data,
-    time_limit=int(time)
-)
+
+models_dir = tempfile.mkdtemp() + os.sep
+init_params = {'label': 'Class', 'eval_metric': 'log_loss', 'path': models_dir, 'problem_type': 'multiclass'}
+train_params = {'train_data': '/home/runner/.cache/openml/org/openml/www/datasets/46871/dataset_train_0.parquet', 'time_limit': 60}
+predictor = TabularPredictor(**init_params).fit(**train_params)
 
 print("Training finished successfully")
-predictions = predictor.predict(test_data)
