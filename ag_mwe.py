cd99347e49c13cc1ab1c03d9eb1db229ab0e0677
Pieter Gijsbers <p.gijsbers@tue.nl>
2025-06-17 10:56:56 +0200

    Update ag_mwe.py

--- ag_mwe.py
+++ ag_mwe.py
@@ -5,14 +5,11 @@ import tempfile
 
 _, time, train, test = sys.argv
 
-train_data = TabularDataset(train)
-test_data = TabularDataset(test)
-
 print("Training AutoGluon")
 
 models_dir = tempfile.mkdtemp() + os.sep
 init_params = {'label': 'Class', 'eval_metric': 'log_loss', 'path': models_dir, 'problem_type': 'multiclass'}
-train_params = {'train_data': '/home/runner/.cache/openml/org/openml/www/datasets/46871/dataset_train_0.parquet', 'time_limit': 60}
+train_params = {'train_data': train, 'time_limit': time}
 predictor = TabularPredictor(**init_params).fit(**train_params)
 
 print("Training finished successfully")
