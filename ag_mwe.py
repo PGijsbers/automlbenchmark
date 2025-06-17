1d0f4dcf5c7d6303fd76b4a06890801500642861
Pieter Gijsbers <p.gijsbers@tue.nl>
2025-06-17 11:09:21 +0200

    Update ag_mwe.py

--- ag_mwe.py
+++ ag_mwe.py
@@ -9,7 +9,7 @@ print("Training AutoGluon")
 
 models_dir = tempfile.mkdtemp() + os.sep
 init_params = {'label': 'Class', 'eval_metric': 'log_loss', 'path': models_dir, 'problem_type': 'multiclass'}
-train_params = {'train_data': train, 'time_limit': time}
+train_params = {'train_data': train, 'time_limit': int(time)}
 predictor = TabularPredictor(**init_params).fit(**train_params)
 
 print("Training finished successfully")
